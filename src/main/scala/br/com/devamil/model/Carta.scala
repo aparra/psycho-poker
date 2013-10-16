8c0293d021daa1b0686ed111b17823073334511b
Anderson Parra <anderson.paula@amil.com.br>
2013-10-16 13:32:06 -0300

    não é possível criar cartas inválidas

--- src/main/scala/br/com/devamil/model/Carta.scala
+++ src/main/scala/br/com/devamil/model/Carta.scala
@@ -1,6 +1,8 @@
 package br.com.devamil.model
 
-case class Carta(valor: Valor, naipe: Naipe)
+case class Carta(valor: Valor, naipe: Naipe) {
+  require(valor != null && naipe != null, "carta.invalida")
+}
 
 object Carta {
   
@@ -9,5 +11,11 @@ object Carta {
   def apply(valor: String): Carta = valor match {
     case pattern(valor, naipe) => new Carta(Valor.from(valor), Naipe.from(naipe))
     case _ => throw new IllegalArgumentException("carta.invalida")  
+  }
+}
+
+object test {
+  def main(args: Array[String]) {
+    println(new Carta(null, null));
   }
 }
--- src/test/scala/br/com/devamil/model/CartaTest.scala
+++ src/test/scala/br/com/devamil/model/CartaTest.scala
@@ -10,11 +10,21 @@ class CartaTest extends Specification {
     val doisPaus = Carta("2C")
 
     "ter o Valor DOIS" in {
-      doisPaus.valor must be equalTo DOIS  
+      doisPaus.valor must be equalTo DOIS
     }
-    
+
     "ser do Naipe PAUS" in {
-      doisPaus.naipe must be equalTo PAUS  
+      doisPaus.naipe must be equalTo PAUS
+    }
+  }
+
+  "Tentar criar uma Carta invalida" should {
+    "lancar IllegalArgumentException para valores invalidos" in {
+      (Carta("69")) must throwA(new IllegalArgumentException("carta.invalida"))
+    }
+
+    "lancar IllegalArgumentException para valores nulos" in {
+      (new Carta(null, null)) must throwAn[IllegalArgumentException]
     }
   }
 }
