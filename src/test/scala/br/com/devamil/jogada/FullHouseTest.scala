2afb0de13c65bf91a7fde8bf5a6e9603af477f52
Anderson Parra <ander.pp@gmail.com>
2013-10-25 09:44:29 -0200

    regra da jogada full house

---
+++ src/test/scala/br/com/devamil/jogada/FullHouseTest.scala
@@ -0,0 +1,24 @@
+package br.com.devamil.jogada
+
+import org.specs2.mutable._
+
+class FullHouseTest extends Specification {
+
+  "A jogada full house" should {
+    "ter o nome 'full-house'" in {
+      FullHouse.nome must be equalTo "full-house"
+    }
+  }
+  
+  {val cartas = Cartas("2C 9C 2H 9S 9D"); cartas.mkString(" ") should {
+    "formar a jogada full house" in {
+      FullHouse.avalia(cartas) must beTrue
+    }
+  }}
+  
+  {val cartas = Cartas("2C 2H 2S 2D 8D"); cartas.mkString(" ") should {
+    "nao forma a jogada full house" in {
+      FullHouse.avalia(cartas) must beFalse
+    }
+  }}
+}
